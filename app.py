abc6c73da5815cbe7ed521e6a1f753116fd526ef
slowerhd <xpreciox@gmail.com>
2021-06-11 22:25:54 -0400

    just a commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+#Importaciones
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
+#Conexión a la BD
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -8,6 +10,7 @@ app.config['MYSQL_PASSWORD'] = 'mysql'
 app.config['MYSQL_DB'] = 'dbprueba'
 mysql = MySQL()
 
+#Recolección de Datos mediante POST, GET, PUT, DELETE
 @app.route('/')
 def Index():
     return 'Esto es una prueba'
